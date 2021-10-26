39ab7140ca355dd5194858d8fa5e3ca2d976a208
khippert5 <khippert@kmi-cms.com>
2021-10-25 18:43:04 -0700

    build form submission lambda

--- create-order/src/lib/types.js
+++ create-order/src/lib/types.js
@@ -1,18 +1,6 @@
 // @flow
 
-export type Order = {
-  billing: string,
-  email: string,
-  products: [
-    {
-      sku: string,
-      quantity: string,
-      options: {
-        sizes: string,
-      },
-    },
-  ],
-  shipping: string,
-  tax: string,
-  total: string,
+export type FormData = {
+  formname: string,
+  [key: string]: string,
 };
