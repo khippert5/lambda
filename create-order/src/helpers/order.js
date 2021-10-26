7c903aaefbf7e00952a8e9d9787619eae0cb139e
Kevin <khippert@kmi-cms.com>
2021-10-26 01:25:54 -0700

    Revert createOrder

---
+++ create-order/src/helpers/order.js
@@ -0,0 +1,21 @@
+// @flow
+import { v4 as uuidv4 } from 'uuid';
+
+import type { Order } from '../lib/types';
+
+export const getOrderPayload = (order: Order): { [key: string]: { S: string } } => {
+  let newOrder = {};
+  // eslint-disable-next-line array-callback-return
+  Object.keys(order).map((prop) => {
+    let value = order[prop];
+    if (order[prop] !== 'string') value = JSON.stringify(order[prop]);
+    newOrder[prop] = { S: value };
+  });
+
+  newOrder.orderNumber = { S: uuidv4() };
+  newOrder.timeStamp = { S: new Date().getTime().toString() };
+
+  return newOrder;
+};
+
+export default getOrderPayload;
